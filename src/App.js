8771d9de94d62c7c9cc234c1dc62411137eee7d4
mayur.l1 <mayur.l@elaunchinfotech.in>
2021-09-01 12:18:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -19,9 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-        <Switch>
-            <Route exact path="/"></Route>
-        </Switch>
     </div>
   );
 }
